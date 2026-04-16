a54acf9d270813cbebd253445f3a3c0eb1eea294
AriZona07 <ariana.torres.of@gmail.com>
2026-04-16 02:17:37 -0600

    Feat + Update: Playlist integrada + update al css

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,30 @@ import './App.css';
 function App() {
   return (
     <div className="general"> 
-      <header>
-        <h1>Título</h1>
+      <header className="header">
+        <h1>Marce</h1>
+        <h2>Esto es para ti, te amo 💕</h2>
+
+        <div>
+          <p>Hola mi niña, hice esta página para ti, para que veas cómo te quiero</p>
+        </div>
       </header>
 
-      <div className="div-centrado">
-        <p>Hola mi niña, hice esta página para ti, para que veas cómo te quiero</p>
+      <div className="div-izquierda">
+        <p>
+          Hice esta playlist para ti cariño,
+          son canciones de amor de los artistas/grupos que te gustan ❤️: {" "}
+          <a href='https://youtube.com/playlist?list=PLFtukRTdAHQI-NC5AYexu__7uE0dHke-t&si=aaimUHLAkgqZkcti'>
+            Playlist
+          </a>
+        </p>
+        <iframe className="youtube-video"
+          src="https://www.youtube.com/embed/videoseries?si=W5_jpfaTOH1lY36Y&amp;list=PLFtukRTdAHQI-NC5AYexu__7uE0dHke-t" 
+          title="Playlist para Marce" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          referrerPolicy="strict-origin-when-cross-origin" 
+          allowFullScreen>
+        </iframe>
       </div>
     </div>
   )
